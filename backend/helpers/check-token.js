54273d19abf385448087b4113510bd999a858c88
ts2017185 <ts2017185@gmail.com>
2021-11-25 01:29:32 +0000

    Schedule Update

--- backend/helpers/check-token.js
+++ backend/helpers/check-token.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const checkToken = (req, res, next) => {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
-
+    console.log(authHeader)
     if (!token) return res.status(401).json({ message: "Access denied!" });
 
     try {
